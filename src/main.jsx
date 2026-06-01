95b8986b2e86bf13c28573f4cd65b676d71e99b9
ahmtilhn <119415449+ahmtilhn@users.noreply.github.com>
2026-06-01 08:02:51 +0200

    Import product hub stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
+import './product-hub.css';
 import './safety.css';
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
